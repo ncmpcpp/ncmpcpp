0c9e10091f1992f00ad67266436dfd9ba507c790
Andrzej Rybczak <electricityispower@gmail.com>
2009-01-17 00:52:08 +0100

    update fsf address and copyright info

--- src/mpdpp.h
+++ src/mpdpp.h
@@ -1,6 +1,6 @@
 /***************************************************************************
- *   Copyright (C) 2008 by Andrzej Rybczak   *
- *   electricityispower@gmail.com   *
+ *   Copyright (C) 2008-2009 by Andrzej Rybczak                            *
+ *   electricityispower@gmail.com                                          *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
  *   it under the terms of the GNU General Public License as published by  *
@@ -15,7 +15,7 @@
  *   You should have received a copy of the GNU General Public License     *
  *   along with this program; if not, write to the                         *
  *   Free Software Foundation, Inc.,                                       *
- *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ *   51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA.              *
  ***************************************************************************/
 
 #ifndef _MPDPP_H
