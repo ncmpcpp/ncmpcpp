12e437c5afddb1c3714514870509c18310c76d2c
Andrzej Rybczak <electricityispower@gmail.com>
2009-02-21 16:48:38 +0100

    really fix compilation --without-curl

--- src/lyrics.cpp
+++ src/lyrics.cpp
@@ -18,6 +18,7 @@
  *   51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA.              *
  ***************************************************************************/
 
+#include <cstring>
 #include <sys/stat.h>
 #include <fstream>
 
