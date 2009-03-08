7f2a635ac59805e29dd35aad8cba4a1aeaad6f99
Andrzej Rybczak <electricityispower@gmail.com>
2009-03-08 15:53:15 +0100

    use inline functions instead of macros in charset.h

--- src/charset.h
+++ src/charset.h
@@ -25,9 +25,9 @@
 #include <config.h>
 #endif
 
-#if defined(SUPPORTED_LOCALES) && defined(HAVE_ICONV_H)
-
 #include <string>
+
+#if defined(SUPPORTED_LOCALES) && defined(HAVE_ICONV_H)
 
 void init_current_locale();
 
@@ -42,16 +42,16 @@ void str_pool_locale_to_utf(char *&);
 
 #else
 
-#define init_current_locale();
+void init_current_locale() { }
 
-#define utf_to_locale(x);
-#define locale_to_utf(x);
+void utf_to_locale(std::string &) { }
+void locale_to_utf(std::string &) { }
 
-#define utf_to_locale_cpy(x) (x)
-#define locale_to_utf_cpy(x) (x)
+std::string &utf_to_locale_cpy(const std::string &s) { return s; }
+std::string &locale_to_utf_cpy(const std::string &s) { return s; }
 
-#define str_pool_utf_to_locale(x);
-#define str_pool_locale_to_utf(x);
+void str_pool_utf_to_locale(char *&) { }
+void str_pool_locale_to_utf(char *&) { }
 
 #endif // SUPPORTED_LOCALES && HAVE_ICONV_H
 
