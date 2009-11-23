d2b9aee650e821b1be404f2865d1d59dac9db3c6
Andrzej Rybczak <electricityispower@gmail.com>
2009-11-23 21:25:33 +0100

    add missing files: tolower.{cpp,h}

---
+++ src/tolower.cpp
@@ -0,0 +1,35 @@
+/***************************************************************************
+ *   Copyright (C) 2008-2009 by Andrzej Rybczak                            *
+ *   electricityispower@gmail.com                                          *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA.              *
+ ***************************************************************************/
+
+#include <algorithm>
+#include <wctype.h>
+
+#include "tolower.h"
+
+void ToLower(std::string &s)
+{
+	transform(s.begin(), s.end(), s.begin(), tolower);
+}
+
+void ToLower(std::wstring &s)
+{
+	transform(s.begin(), s.end(), s.begin(), towlower);
+}
+
