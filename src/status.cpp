055cf43af3a5a3911c94c5076bc60adea1836b51
Andrzej Rybczak <electricityispower@gmail.com>
2009-12-19 00:19:14 +0100

    handle update "properly" (as it's possible) with <mpd-0.16
    <mpd-0.16 doesn't support MPD_IDLE_UPDATE, so we need another
    extra hack to support that. This whole idle things really starts
    to pissing me off.

--- src/status.cpp
+++ src/status.cpp
@@ -519,8 +519,11 @@ void NcmpcppStatusChanged(Connection *, StatusChanges changed, void *)
 	}
 	if (changed.DBUpdating)
 	{
-		mpd_db_updating = Mpd.GetDBIsUpdating() ? 'U' : 0;
-		ShowMessage(!mpd_db_updating ? "Database update finished!" : "Database update started!");
+		// mpd-0.{14,15} doesn't support idle notification that dbupdate had
+		// finished and nothing changed, so we need to switch it off for them.
+		if (Mpd.Version() < 14 || Mpd.Version() > 15)
+			mpd_db_updating = Mpd.GetDBIsUpdating() ? 'U' : 0;
+		ShowMessage(Mpd.GetDBIsUpdating() ? "Database update finished!" : "Database update started!");
 	}
 	if (changed.StatusFlags && (Config.header_visibility || Config.new_design))
 	{
