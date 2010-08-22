6af99edcc14b85859cf35dfa07964d70105d4e0b
Andrzej Rybczak <electricityispower@gmail.com>
2010-08-22 20:58:34 +0200

    conv: shorten: maintain proper string length if max_length is odd

--- src/conv.cpp
+++ src/conv.cpp
@@ -222,7 +222,7 @@ std::string Shorten(const std::basic_string<my_char_t> &s, size_t max_length)
 		return TO_STRING(s);
 	if (max_length < 2)
 		return "";
-	std::basic_string<my_char_t> result(s, 0, max_length/2-1);
+	std::basic_string<my_char_t> result(s, 0, max_length/2-!(max_length%2));
 	result += U("..");
 	result += s.substr(s.length()-max_length/2+1);
 	return TO_STRING(result);
