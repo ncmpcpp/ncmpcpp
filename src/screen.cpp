bc9d04ffe340563498dfd348b641c76ec36c22c6
Andrzej Rybczak <electricityispower@gmail.com>
2012-01-28 23:17:02 +0100

    screen: fix isVisible function

--- src/screen.cpp
+++ src/screen.cpp
@@ -51,7 +51,7 @@ bool isVisible(BasicScreen *screen)
 {
 	assert(screen != 0);
 	if (myLockedScreen && myScreen->isMergable())
-		return screen == myInactiveScreen || screen == myLockedScreen;
+		return screen == myScreen || screen == myInactiveScreen || screen == myLockedScreen;
 	else
 		return screen == myScreen;
 }
