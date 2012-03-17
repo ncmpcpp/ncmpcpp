f8151904f5842e426822eaa40802dfa30f4b7f9e
Andrzej Rybczak <electricityispower@gmail.com>
2012-03-17 16:46:55 +0100

    update copyright dates

--- src/display.h
+++ src/display.h
@@ -1,5 +1,5 @@
 /***************************************************************************
- *   Copyright (C) 2008-2011 by Andrzej Rybczak                            *
+ *   Copyright (C) 2008-2012 by Andrzej Rybczak                            *
  *   electricityispower@gmail.com                                          *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
