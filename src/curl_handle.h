4ac2990baf29485c80a02fb8296dd926e4b34ffc
Eric Izoita <ericizoita@gmail.com>
2015-09-01 20:14:00 -0700

    lyrics fetcher: fix lyricwiki.com redirect issue

--- src/curl_handle.h
+++ src/curl_handle.h
@@ -30,7 +30,7 @@
 
 namespace Curl
 {
-	CURLcode perform(std::string &data, const std::string &URL, const std::string &referer = "", unsigned timeout = 10);
+	CURLcode perform(std::string &data, const std::string &URL, const std::string &referer = "", bool follow_redirect = false, unsigned timeout = 10);
 	
 	std::string escape(const std::string &s);
 }
--- src/lyrics_fetcher.cpp
+++ src/lyrics_fetcher.cpp
@@ -118,7 +118,7 @@ LyricsFetcher::Result LyricwikiFetcher::fetch(const std::string &artist, const s
 		result.first = false;
 		
 		std::string data;
-		CURLcode code = Curl::perform(data, result.second);
+		CURLcode code = Curl::perform(data, result.second, "", true);
 		
 		if (code != CURLE_OK)
 		{
