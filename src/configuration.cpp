9355075dca0075b775c530a4d4e8189940ec12c4
ayekat <takeya@bluewin.ch>
2016-12-03 19:25:50 +0100

    Store error.log to $XDG_CACHE_HOME by default
    Also add configuration option to change the location, if the user does
    not wish to have log files stored in $XDG_CACHE_HOME.

--- src/configuration.cpp
+++ src/configuration.cpp
@@ -192,6 +192,7 @@ bool configure(int argc, char **argv)
 
 		// create directories
 		boost::filesystem::create_directory(Config.ncmpcpp_directory);
+		boost::filesystem::create_directory(Config.logs_directory);
 		boost::filesystem::create_directory(Config.lyrics_directory);
 
 		// try to get MPD connection details from environment variables
