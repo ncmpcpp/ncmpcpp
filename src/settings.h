dba68adba85cb4ada1160f38866d73752e6462a4
Daniel Lublin <daniel@lublin.se>
2020-10-29 09:43:43 +0100

    Add exclude_pattern option
    Fixes https://github.com/ncmpcpp/ncmpcpp/issues/422

--- src/settings.h
+++ src/settings.h
@@ -222,6 +222,7 @@ struct Configuration
 	std::vector<ScreenType> screen_sequence;
 
 	std::string random_exclude_pattern;
+	std::string exclude_pattern;
 	SortMode browser_sort_mode;
 
 	LyricsFetchers lyrics_fetchers;
