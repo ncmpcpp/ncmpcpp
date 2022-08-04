1ee9faf7af4e4b73eb936036a01b2325600e6e32
self <george@gabbott.dev>
2022-08-04 21:20:37 +0100

    corrected spaces to tabs

--- src/actions.cpp
+++ src/actions.cpp
@@ -1771,15 +1771,15 @@ void JumpToPositionInSong::run()
 		int secs = (percent * s.getDuration()) / 100.0;
 		Mpd.Seek(s.getPosition(), secs);
 	}
-    else if (boost::regex_match(spos, what, rx.assign("([0-9]+):([0-9]{1,2}):([0-9]{2})"))) // position in hh:mm:ss 
+	else if (boost::regex_match(spos, what, rx.assign("([0-9]+):([0-9]{1,2}):([0-9]{2})"))) // position in hh:mm:ss 
     {
-        auto hours = fromString<unsigned>(what[1]);
-        auto mins  = fromString<unsigned>(what[2]);
-        auto secs  = fromString<unsigned>(what[3]);
-        boundsCheck(mins, 0u, 60u);
-        boundsCheck(secs, 0u, 60u);
-        Mpd.Seek(s.getPosition(), hours * 3600 + mins * 60 + secs);
-    }
+		auto hours = fromString<unsigned>(what[1]);
+		auto mins  = fromString<unsigned>(what[2]);
+		auto secs  = fromString<unsigned>(what[3]);
+		boundsCheck(mins, 0u, 60u);
+		boundsCheck(secs, 0u, 60u);
+		Mpd.Seek(s.getPosition(), hours * 3600 + mins * 60 + secs);
+	}
 	else
 		Statusbar::print("Invalid format ([h]:[m]:[ss], [m]:[ss], [s]s, [%]%, [%] accepted)");
 }
