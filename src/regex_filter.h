cd9ae93c3e7ce34c564bb637c043647164f92105
Andrzej Rybczak <andrzej@rybczak.net>
2024-08-21 18:18:51 +0200

    Fix boost::regex ICU support

--- src/regex_filter.h
+++ src/regex_filter.h
@@ -93,7 +93,7 @@ inline Regex make(StringT &&s,
 template <typename CharT>
 inline bool search(const std::basic_string<CharT> &s,
                    const Regex &rx,
-                   [[maybe_unused]] bool ignore_diacritics)
+                   bool ignore_diacritics)
 {
 	try {
 #ifdef BOOST_REGEX_ICU
